010f29688fbd028dde521b8b449eaefb7d3fa4bb
luiza120 <134612996+luiza120@users.noreply.github.com>
2024-10-01 11:37:36 -0300

    Add files via upload

---
+++ heart.py
@@ -0,0 +1,11 @@
+from turtle import *
+color ('red')
+begin_fill()
+pensize(3)
+left(50)
+forward(133)
+circle( radius= 50, extent= 200)
+right(140)
+circle( radius= 50, extent= 200)
+forward(133)
+end_fill()
